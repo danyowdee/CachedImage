4011064dcd312c28a4b91ab08f463a5af0c3a03d
Daniel Demiss <daniel@dock12.net>
2012-11-22 22:43:46 +0100

    Initial Commit

---
+++ CachedImage/CachedImage.h
@@ -0,0 +1,13 @@
+//
+//  CachedImage.h
+//  CachedImage
+//
+//  Created by Daniel Demiss on 22.11.12.
+//  Copyright (c) 2012 Daniel Demiss. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CachedImage : NSObject
+
+@end
---
+++ CachedImage/CachedImage.m
@@ -0,0 +1,13 @@
+//
+//  CachedImage.m
+//  CachedImage
+//
+//  Created by Daniel Demiss on 22.11.12.
+//  Copyright (c) 2012 Daniel Demiss. All rights reserved.
+//
+
+#import "CachedImage.h"
+
+@implementation CachedImage
+
+@end
