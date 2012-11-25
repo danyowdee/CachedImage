029040bd574c69800820f9e9963102a5ed30a4bf
Daniel Demiss <daniel@dock12.net>
2012-11-25 22:38:46 +0100

    Added the missing headerdoc for the method.

--- CachedImage/Source/UIImage+D12_SecondaryBundleCacheSupport.h
+++ CachedImage/Source/UIImage+D12_SecondaryBundleCacheSupport.h
@@ -18,6 +18,21 @@
 
 @interface UIImage (D12_SecondaryBundleCacheSupport)
 
+/**
+ Returns the image object associated with the specified filename from the specified bundle.
+
+ This method looks in the system caches for an image object with the specified name and returns that object if it exists.
+ If a matching image object is not already in the cache, this method loads the image data from the specified file, caches it, and then returns the resulting object.
+
+ Like “The Real Deal™”, this method attempts loading @2x versions on retina devices.
+ In fact, if bundle is `nil`, or the main bundle, this method uses the standard `+[UIImage imageNamed:]`.
+
+ @param  name    The name of the file.
+ If this is the first time the image is being loaded, the method looks for an image with the specified name in the application’s main bundle.
+ @param  bundle  The bundle from which to load the image.
+
+ @return The image object for the specified file from the specified bundle, or `nil` if the method could not find the specified image.
+ */
 + (UIImage *)d12_imageNamed:(NSString *)name inBundle:(NSBundle *)bundle;
 
 @end
